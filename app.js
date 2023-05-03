924042d9ec101690774032a41307cad09e51cacd
voton <voton240+github@gmail.com>
2023-05-03 02:17:35 +0200

    Przebudowano kod aplikacji
    Przepisano na nowo:
     - app.js
     - index.hbs

--- app.js
+++ app.js
@@ -1,33 +1,57 @@
 const express = require('express')
 const fs = require('fs')
 
+const utils = require('./modules/utils')
+
 const port = 3000
 const app = express()
-
-const loadImages = () =>{
-    imageList = []
-
-    images = fs.readdirSync("./public/lib/")
-    for(let i = 0; i < images.length; i++){
-        imageList.push(images[i])
-    }
-
-    console.log(imageList)
-
-    return JSON.stringify(imageList)
-}
 
 app.set('view engine', 'hbs')
 app.use('/public', express.static('public'))
 
+const load_boards = () => {
+    let list = []
+
+    boards = fs.readdirSync('./public/library/')
+    for(let i = 0; i < boards.length; i++){
+        list.push(boards[i])
+    }
+
+    return list
+}
+const load_board = (data) => {
+    let list = []
+    if(typeof(data) == "string"){  
+        images = fs.readdirSync(`./public/library/${data}/`)
+        for(let i = 0; i < images.length; i++){
+            list.push([data, images[i]])
+        }
+    }
+    else{
+        for(let i = 0; i < data.length; i++){
+            images = fs.readdirSync(`./public/library/${data[i]}/`)
+            for(let j = 0; j < images.length; j++){
+                list.push([data[i], images[j]])
+            }
+        }
+    }
+
+    return list
+}
+
 app.get('/', function(req, res){
-    res.render('index',{
-        imageList: loadImages()
+    const board_list = load_boards()
+    const boardID = req.query.boardID
+
+    let board = board_list[boardID]
+    if(board == undefined) board = board_list
+
+    const image_set = load_board(board)
+    res.render('index', {
+        board_list: JSON.stringify(board_list),
+        image_set: JSON.stringify(image_set)
     })
 })
 
-console.log("Server status: Running")
-console.log(` - App: ${ __dirname.split('\\')[__dirname.split('\\').length - 1] }`)
-console.log(` - Port: ${ port }`)
-
+utils.server_log(__dirname, port)
 app.listen(port)
---
+++ modules/utils.js
@@ -0,0 +1,69 @@
+/// Version 1.1.0
+const fs = require('fs')
+
+/* Math */
+const max_value = (max, val) => {
+    if (max > val) val = max
+    return val
+}
+
+// Random
+const random_number = (len) => {
+    return Math.floor(Math.random() * len)
+}
+const random_range = (max) => {
+    return Math.floor(Math.random() * max) + 1
+}
+
+/* Data */
+// Array
+const shuffle = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i + 1))
+        const temp = arr[i]
+
+        arr[i] = arr[j]
+        arr[j] = temp
+    }
+
+    return arr
+}
+const random_array = (arr) => {
+    return arr[random_number(arr.length)]
+}
+
+// Json
+const load_json = (dir) => {
+    const data_raw = fs.readFileSync(dir)
+    const data_json = JSON.parse(data_raw)
+
+    return data_json
+}
+
+/* String */
+const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/* Other */
+const project_name = (__dirname) => {
+    return __dirname.split('\\')[__dirname.split('\\').length - 1]
+}
+
+const server_log = (__dirname, port) => {
+    console.log("Server status: Running")
+    console.log(` - App: ${ capitalize(project_name(__dirname)) }`)
+    console.log(` - Port: ${ port }`)
+}
+
+/* Export */
+module.exports = {
+    random_number,
+    random_array,
+    random_range,
+    capitalize,
+    server_log,
+    load_json,
+    max_value,
+    shuffle
+}
